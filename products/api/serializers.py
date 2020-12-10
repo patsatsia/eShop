35c196a9c2b628d98981dbb1b682957c023937d2
Giorgi Patsatsia <pats.giorgi@gmail.com>
2020-12-10 12:38:20 +0400

    gg

--- products/api/serializers.py
+++ products/api/serializers.py
@@ -2,37 +2,29 @@ from rest_framework import serializers
 from products.models import Product
 from category.models import Category
 from category.api.serializers import CategorySerializer
+from django.contrib.auth.models import User
+from dj_rest_auth.serializers import LoginSerializer, UserDetailsSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(many=True)
-
-
-    created_by = serializers.StringRelatedField()
-    updated_by = serializers.StringRelatedField()
-    deleted_by = serializers.StringRelatedField()
-    
+    category = CategorySerializer(many=True, required=False)  
+    category_ids = serializers.PrimaryKeyRelatedField(source='category', many=True, queryset=Category.objects.all())
+    created_by = UserDetailsSerializer(required=False)
+    updated_by = UserDetailsSerializer(read_only=True)
+    deleted_by = UserDetailsSerializer(read_only=True)
 
     class Meta:
         model = Product
         fields = (
-            "id", "name", "category", "description", "price", 
+            "id", "name", "category_ids", "category", "description", "price", 
             "quantity", "created_at", "created_by", "updated_at", 
             "updated_by", "deleted_at", "deleted_by",
         )
-
-    # def create(self, validated_data):
-    #     category_data = validated_data.pop('category')
-    #     product = Product.objects.create(**validated_data)
-    #     for category in category_data:
-    #         category, created = Category.objects.get_or_create(name=category["name"])
-    #         product.category.add(category)
-    #     return product
+        # depth = 1
 
     def create(self, validated_data):
-        category_data = validated_data.pop('category')
+        validated_data.pop("category", [])
         product = Product.objects.create(**validated_data)
-        for category in category_data:
-            Category.objects.create(product=product, **category_data)
-
-        return product  
+        category = Category.objects.get(pk = category_ids)
+        product.category.adsetd(category)
+        return product
