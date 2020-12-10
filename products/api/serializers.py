fd02ff5caf29eb92eff165f5cb037b2042bd591e
Giorgi Patsatsia <pats.giorgi@gmail.com>
2020-12-10 22:56:17 +0400

    create & update

--- products/api/serializers.py
+++ products/api/serializers.py
@@ -5,13 +5,12 @@ from category.api.serializers import CategorySerializer
 from django.contrib.auth.models import User
 from dj_rest_auth.serializers import LoginSerializer, UserDetailsSerializer
 
-
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(many=True, required=False)  
+    category     = CategorySerializer(many=True, required=False, read_only=True)  
     category_ids = serializers.PrimaryKeyRelatedField(source='category', many=True, queryset=Category.objects.all())
-    created_by = UserDetailsSerializer(required=False)
-    updated_by = UserDetailsSerializer(read_only=True)
-    deleted_by = UserDetailsSerializer(read_only=True)
+    created_by   = UserDetailsSerializer(required=False)
+    updated_by   = UserDetailsSerializer(required=False)
+    deleted_by   = UserDetailsSerializer(required=False)
 
     class Meta:
         model = Product
@@ -23,8 +22,30 @@ class ProductSerializer(serializers.ModelSerializer):
         # depth = 1
 
     def create(self, validated_data):
-        validated_data.pop("category", [])
+        validated_data.pop("category")
+        category_ids = self.context.get('request').data.get('category_ids')
         product = Product.objects.create(**validated_data)
-        category = Category.objects.get(pk = category_ids)
-        product.category.adsetd(category)
+        for item in category_ids:
+            category = Category.objects.get(id=item)
+            product.category.add(category)
         return product
+
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data["name"]
+        instance.price = validated_data["price"]
+        instance.description = validated_data["description"]
+        instance.quantity = validated_data["quantity"]
+
+        instance.updated_by = self.context['request'].user #????????
+
+        category_ids = self.context.get('request').data.get('category_ids')
+        instance.category.set(category_ids)
+
+        # instance.category.clear()
+        # for item in category_ids:
+        #     category = Category.objects.get(id=item)
+        #     instance.category.add(item)        
+        instance.save()
+        
+        return instance
