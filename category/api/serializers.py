6daabe03f0879657b7ea97773fd1c29b43e86fa6
Giorgi Patsatsia <pats.giorgi@gmail.com>
2020-12-11 16:27:22 +0400

    updated category, changed timezon

--- category/api/serializers.py
+++ category/api/serializers.py
@@ -1,8 +1,13 @@
 from category.models import Category
 from rest_framework import serializers
 from rest_framework.serializers import SerializerMetaclass
+from dj_rest_auth.serializers import UserDetailsSerializer
 
 class CategorySerializer(serializers.ModelSerializer):
+    created_by = UserDetailsSerializer(required=False)
+    updated_by = UserDetailsSerializer(required=False)
+    deleted_by = UserDetailsSerializer(required=False)
+
    
     class Meta:
         model = Category
