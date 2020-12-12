378ce8815c967b408cf0ff620638f2c508398f9d
Giorgi Patsatsia <pats.giorgi@gmail.com>
2020-12-12 16:49:49 +0400

    models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "{}'s Cart: {} ({})".format(self.user, self.product, self.quantity)
